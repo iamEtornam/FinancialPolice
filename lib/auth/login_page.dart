73e365bbdb525d381ae2285ecdcdfce06a6c27f9
regnex <sunumacbright@gmail.com>
2019-01-11 15:24:22 +0000

    update: added login Page with phone auth

---
+++ lib/auth/login_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     body: Column(
+       crossAxisAlignment: CrossAxisAlignment.stretch,
+       mainAxisAlignment: MainAxisAlignment.center,
+       mainAxisSize: MainAxisSize.max,
+       children: <Widget>[
+
+       ],
+     ),
+    );
+  }
+}
