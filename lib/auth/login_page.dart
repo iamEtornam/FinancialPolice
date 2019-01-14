e8b97d834de6d74062ece91529b0f9cec1924956
regnex <sunumacbright@gmail.com>
2019-01-14 12:05:59 +0000

    fix: blur background images

--- lib/auth/login_page.dart
+++ lib/auth/login_page.dart
@@ -12,18 +12,21 @@ class _LoginPageState extends State < LoginPage > {
     return Scaffold(
       body: Center(
         child: Container(
-          decoration: new BoxDecoration(color: Colors.black45),
-         child: BackdropFilter(
-           filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
-           child: Image.asset('assets/images/bg_image.jpg',
-         fit: BoxFit.cover,
-         height: double.infinity,
-         width: double.infinity,
-         alignment: Alignment.center,
-         ),
-         )
+        decoration: new BoxDecoration(
+          image: new DecorationImage(
+            image: new ExactAssetImage('assets/images/bg_image.jpg'),
+            fit: BoxFit.cover,
+          ),
         ),
+        child: new BackdropFilter(
+          filter: new ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+          child: new Container(
+            decoration: new BoxDecoration(color: Colors.white.withOpacity(0.0)),
+          ),
+        ),
+      ),
       )
     );
   }
 }
+
